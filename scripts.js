fc65d803416a08f124dced6b679be557ef5eada9
ZODIAC 911 <zodiacmadafac911@gmail.com>
2025-02-25 00:04:02 +0100

    avatar image

--- scripts.js
+++ scripts.js
@@ -7,6 +7,33 @@ const githubDisplay = document.querySelectorAll(".github");
 const form = document.getElementById("Form");
 const step1 = document.getElementById("step-1");
 const step2 = document.getElementById("step-2");
+const dateSpan = document.querySelectorAll(".date");
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+const avatar = document.getElementById("userPictur");
+const inputFile = document.getElementById("file");
+let img = new Image();
+
+inputFile.addEventListener("change", (e) => {
+  var selectedFile = e.target.files[0];
+  var reader = new FileReader();
+  reader.onload = (evt) => {
+    avatar.src = evt.target.result;
+  };
+  reader.readAsDataURL(selectedFile);
+});
 
 fullName.addEventListener("change", function (event) {
   fullNamesDisplay.forEach((e) => {
@@ -22,6 +49,12 @@ Github.addEventListener("change", function (event) {
   githubDisplay.forEach((e) => {
     e.innerHTML = event.target.value;
   });
+  dateSpan.forEach((e) => {
+    const d = new Date();
+    console.log(d.getMonth());
+    e.innerText =
+      months[d.getMonth()] + " " + d.getDate() + " ," + d.getFullYear();
+  });
 });
 
 form.addEventListener("submit", function (event) {
@@ -29,3 +62,5 @@ form.addEventListener("submit", function (event) {
   step1.classList.add("hidden");
   step2.classList.remove("hidden");
 });
+
+function parsDate() {}
